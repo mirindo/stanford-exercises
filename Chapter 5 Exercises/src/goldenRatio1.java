32e7f2cbfe823087e30490393abaa4fb33099066
Miroslav <>
2018-04-30 11:45:53 +0200

    first exercise finished

--- Chapter 5 Exercises/src/goldenRatio1.java
+++ Chapter 5 Exercises/src/goldenRatio1.java
@@ -4,10 +4,11 @@ import acm.program.*;
 public class goldenRatio1 extends ConsoleProgram {
 	
 	public void run() {
-		
-		
+		//1 
+		double goldenRatio = (1.0 + Math.sqrt(5.0)) / 2.0;
+		println(goldenRatio);
 		
 	}
-
+	
 
 }
