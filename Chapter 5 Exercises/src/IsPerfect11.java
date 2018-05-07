4e35ef6c0ae7e01758e7842ec6964210c636f312
Miroslav <>
2018-05-07 15:29:35 +0200

    Exercise 11

--- Chapter 5 Exercises/src/IsPerfect11.java
+++ Chapter 5 Exercises/src/IsPerfect11.java
@@ -2,14 +2,30 @@ import acm.program.*;
 
 public class IsPerfect11 extends ConsoleProgram {
 	public void run() {
-		
+//		int givenNumber = readInt("Enter number to know, if its perfect: ");
+//		if (isPerfect(givenNumber)) {
+//			println("Number is perfect");
+//		} else {
+//			println("Number is not perfect");
+//		}
+		for (int i = 1; i <= 9999; i++) {
+			if (isPerfect(i)) println(i);
+		}
 	}
 	
 	private boolean isPerfect(int n) {
-		boolean result = false;
+		boolean result = true;
+		int perfectNumberSum = 0;
 		int number = 1;
+		
 		while (result) {
-			
+			if (n % number == 0) perfectNumberSum += number;
+			number++;
+			if (perfectNumberSum == n) break;
+			if (number == n) {
+				result = false; 
+				break;
+			}
 		}
 		return result;
 	}
